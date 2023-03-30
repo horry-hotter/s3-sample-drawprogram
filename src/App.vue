a735c46797840e00be05d4a7f2e4d407f2510e78
horry-hotter <vendetta7kotopokeby@gmail.com>
2023-03-31 02:54:42 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <DrawTool />
+  </div>
+</template>
+
+<script>
+import DrawTool from './components/DrawTool.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DrawTool
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
